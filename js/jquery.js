c0c8ef21d83aee7deb4969e36d38c0e58740a35d
佐竹　宏平 <koheisatake@gmail.com>
2017-11-01 15:03:34 +0900

    header-openのjQuery実装

---
+++ js/jquery.js
@@ -0,0 +1,16 @@
+		$(function() {
+		    var $header = $('#top-head');
+		    // Nav Fixed
+		    $(window).scroll(function() {
+		        if ($(window).scrollTop() > 350) {
+		            $header.addClass('fixed');
+		        } else {
+		            $header.removeClass('fixed');
+		        }
+	        });
+		        // Nav Toggle Button
+		    $('#nav-toggle').click(function(){
+	            $header.toggleClass('open');
+	        });
+		})(jQuery);
+
